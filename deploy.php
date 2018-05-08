cad02a276f727323f3461fcaa276297d25737694
Patrick Moon <pmoon@letsgolearn.com>
2018-05-08 15:49:00 -0700

    adds deploy
    needs this file to run the automated deploy

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'Filaments');
+
+// Project repository
+set('repository', 'git@github.com:mrdatawolf/Filaments.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('filaments.guru')
+  ->user('deployer')
+  ->identityFile('~/.ssh/deployerkey')
+  ->set('deploy_path', '/var/www/html/laravel-app');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
